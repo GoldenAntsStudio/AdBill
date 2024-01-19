a35907048ae8eb0bfd51c34fa5ed89ed4eba5b64
Smart Ant Studio <hometikhon777@gmail.com>
2024-01-19 12:28:49 +0300

    1.3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template
 import os
 
-app = Flask(__name__, template_folder="/Templates")
+app = Flask(__name__, template_folder="./Templates")
 
 
 @app.route('/')
